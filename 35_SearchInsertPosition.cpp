6f454add088b8478149d1989d6b1565fa8e835e0
Aayush Kalia <83601717+aayushkalia@users.noreply.github.com>
2025-02-01 17:17:53 +0000

    the commit code was entered

--- 35_SearchInsertPosition.cpp
+++ 35_SearchInsertPosition.cpp
@@ -11,33 +11,33 @@ public:
         return low;
     }
 };
-<!-- # Intuition -->
-<!-- Describe your first thoughts on how to solve this problem. -->
+// <!-- # Intuition -->
+// <!-- Describe your first thoughts on how to solve this problem. -->
 
-# Approach
-There is a search going on dividing the array in half each time an element gets searched.
+// # Approach
+// There is a search going on dividing the array in half each time an element gets searched.
 
-# Complexity
-- Time complexity:
-o(logn)
+// # Complexity
+// - Time complexity:
+// o(logn)
 
-<!-- - Space complexity: -->
-<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+// <!-- - Space complexity: -->
+// <!-- Add your space complexity here, e.g. $$O(n)$$ -->
 
-# Code
-```cpp []
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int low = 0,high = nums.size()-1,mid = 0;
-        while(low<=high){
-            mid =low+(high-low)/2;
-            if(nums[mid]==target){ return mid;}
-            else if(nums[mid]>target){high=mid-1;}
-            else{low = mid+1;}
-        }
+// # Code
+// ```cpp []
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int low = 0,high = nums.size()-1,mid = 0;
+//         while(low<=high){
+//             mid =low+(high-low)/2;
+//             if(nums[mid]==target){ return mid;}
+//             else if(nums[mid]>target){high=mid-1;}
+//             else{low = mid+1;}
+//         }
 
-        return low;
-    }
-};
-```
+//         return low;
+//     }
+// };
+// ```
