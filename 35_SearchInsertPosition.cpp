9cf8372737623aee501786bffebbf75ff1e17030
Aayush Kalia <83601717+aayushkalia@users.noreply.github.com>
2025-02-08 14:39:26 +0000

    random

--- 35_SearchInsertPosition.cpp
+++ 35_SearchInsertPosition.cpp
@@ -6,7 +6,10 @@ public:
             mid =low+(high-low)/2;
             if(nums[mid]==target){ return mid;}
             else if(nums[mid]>target){high=mid-1;}
-            else{low = mid+1;}
+            else{
+                low = mid+1;
+            }
+
         }
         return low;
     }
